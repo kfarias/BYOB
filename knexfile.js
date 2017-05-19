9eb0241fe6c13ba0c7a0e3a2f2eb06914c33a68b
Kenia Farias <11431kenia@gmail.com>
2017-05-19 14:06:44 -0600

    key added

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,28 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || '?ssl=true',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || '?ssl=true',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
 
   test: {
     client: 'pg',
--- server.js
+++ server.js
@@ -17,16 +17,13 @@ const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.locals.title = 'Diversity Tracker';
 app.use(express.static('public'));
+app.locals.title = 'Diversity Tracker';
 
-app.set('port', process.env.PORT || 3000);
-app.set('secretKey', config.CLIENT_SECRET);
-
-if (!config.CLIENT_SECRET || !config.USERNAME || !config.PASSWORD) {
-  throw 'Make sure you have a CLIENT_SECRET, USERNAME, and PASSWORD in your .env file';
-}
+app.set('secretKey', process.env.CLIENT_SECRET || config.CLIENT_SECRET);
 const token = jwt.sign('user', app.get('secretKey'));
+app.set('port', process.env.PORT || 3000);
+
 console.log(token);
 
 
